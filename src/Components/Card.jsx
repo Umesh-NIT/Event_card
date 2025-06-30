ecd34b88f4694173e79593ec02197e1816ecf954
Umesh-NIT <amarpuraumeshkumarpatel@gmail.com>
2025-06-30 19:59:27 +0530

    edit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "./Card.css";
 
 export const Card = ({
@@ -8,20 +7,37 @@ export const Card = ({
   title,
   description,
   buttonText,
+  buttonIcon,
+  buttonIcon2,
   link,
 }) => {
   return (
     <div className="card-container">
-      {imgSrc && imgAlt && (
+      {imgSrc && (
         <img src={imgSrc} alt={imgAlt} className="card-img" />
       )}
-      {title && <h1 className="card-title">{title}</h1>}
-      {description && <p className="card-description">{description}</p>}
-      {buttonText && link && (
-        <a href={link} className="card-btn">
-          {buttonText}
-        </a>
-      )}
+
+      <div className="card-content">
+        {title && <h1 className="card-title">{title}</h1>}
+        {description && <p className="card-description">{description}</p>}
+        <div className="card-buttons">
+          {buttonText && link && (
+            <a href={link} className="card-btn">
+              {buttonText}
+            </a>
+          )}
+          {buttonIcon && link && (
+            <a href={link} className="btn">
+              {buttonIcon}
+            </a>
+          )}
+          {buttonIcon2 && link && (
+            <a href={link} className="btn">
+              {buttonIcon2}
+            </a>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
